ff34fd27230b145b4a5513d6fced206c30384094
unknown <mohan.enixs@gmail.com>
2024-07-02 16:15:25 +0530

    added endpoints for comments and ratings

--- api/controllers/ratings.controller.js
+++ api/controllers/ratings.controller.js
@@ -16,7 +16,6 @@ export const getComments = async (req,res) => {
     }
 }
 
-
 export const getRatings = async (req,res) => {
     const {postId} = req.body
      try{
@@ -38,14 +37,36 @@ export const addComment = async (req, res) => {
     const { postId, content } = req.body
     const userId = req.userId
     try {
-        const res = await prisma.comment.create({
-            data: {
+        const alreadyExists = await prisma.comment.findFirst({
+            where:{
                 postId,
-                content,
                 userId
-            }
+            },
         })
-        res.status(200).json(res)
+        if(!alreadyExists){
+            const newComment = await prisma.comment.create({
+                data: {
+                    postId,
+                    content,
+                    userId
+                }
+            })
+         res.status(200).json(newComment)
+        }
+        if(alreadyExists){
+            const updateComment = await prisma.comment.update({
+                where:{
+                    id:alreadyExists.id,
+                    userId,
+                    postId
+                },
+                data:{
+                    content
+                }
+            })
+            res.status(200).json(updateComment)
+        }
+      
     } catch (err) {
         console.log(err)
         res.status(500).json({ message: "Oops ! something went wrong !" })
@@ -56,14 +77,38 @@ export const addRating = async (req, res) => {
     const { postId, stars } = req.body
     const userId = req.userId
     try {
-        const rating = await prisma.rating.create({
-            data: {
-                stars,
-                postId,
-                userId
+        const alreadyExists = await prisma.rating.findFirst({
+            where:{
+                postId:postId,
+                userId:userId
             }
         })
-        res.status(200).json(rating)
+        console.log('exists already',alreadyExists)
+        if(alreadyExists){
+            const updateRating = await prisma.rating.update({
+                where:{
+                    id:alreadyExists.id,
+                    postId,
+                    userId
+                },
+                data:{
+                    stars
+                }
+            })
+            res.status(200).json(updateRating)
+        }
+        if(!alreadyExists){
+           const addRating =  await prisma.rating.create({
+                data: {
+                    stars,
+                    postId,
+                    userId
+                }
+            })
+            res.status(200).json(addRating)
+        }
+        
+      
     } catch (err) {
         console.log(err)
         res.status(500).json({ message: "oops ! something went wrong !" })
