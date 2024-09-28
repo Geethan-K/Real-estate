89a187037b3024d40f2201b9dd8966b142bf617c
Geethan-K <mohan.enixs@gmail.com>
2024-09-28 12:52:14 +0530

    added rpn run dev configs in package.json files

--- api/app.js
+++ api/app.js
@@ -13,6 +13,7 @@ import ratingsRoute from './routes/ratings.route.js'
 
 const PORT=3000
 
+//app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
 app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
 app.use(express.json())
 app.use(cookieParser())
--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -75,7 +75,7 @@ export const getPosts = async (req,res) =>{
                 }
             })
             
-        //  console.log(posts)
+          console.log(posts)
         res.status(200).json(posts)
     }catch (err){
         console.log(err);
